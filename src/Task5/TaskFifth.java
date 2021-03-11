0188590c049f6f6c02a2a538609fe7180544df71
catherine-minaieva <75452152+catherine-minaieva@users.noreply.github.com>
2021-03-11 18:36:52 +0200

    Changed Fifth

--- src/Task5/TaskFifth.java
+++ src/Task5/TaskFifth.java
@@ -1,8 +1,6 @@
 package Task5;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -10,11 +8,16 @@ public class TaskFifth {
     private static Stream<String> first = Arrays.stream(new String[]{"A", "B", "C"});
     private static Stream<String> second = Arrays.stream(new String[]{"D", "E", "F", "G", "H", "I"});
 
-    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
-        List<T> result = Stream.concat(first, second).collect(Collectors.toList());
-        Collections.shuffle(result);
-        return result.stream();
-    }
+        public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+            Iterator<T> firstIterate = first.iterator();
+            Iterator<T> secondIterate = second.iterator();
+            Stream<T> result = Stream.empty();
+            while (firstIterate.hasNext() & secondIterate.hasNext()) {
+                result = Stream.concat(result, Stream.of(firstIterate.next(),
+                        secondIterate.next()));
+            }
+            return result;
+        }
 
     public static void main(String[] args) {
         System.out.println(zip(first, second).collect(Collectors.joining(" ")));
